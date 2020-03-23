e5aa32c027c5fd4a7dab1b79b504c6f031591070
seannajera <n112358@gmail.com>
2020-03-22 21:11:31 -0400

    create manage user endpoint

---
+++ app/core/management/commands/create_admin.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    """Django command to pause app execution until db is available"""
+
+    def handle(self, *args, **options):
+        self.stdout.write("Creating Chonio Admin")
+
+        get_user_model().objects.create_superuser(
+            'chonio@admin.com',
+            '00000000'
+        )
+
+        self.stdout.write(self.style.SUCCESS("Admin Chonio Created"))
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,6 +6,7 @@ from rest_framework.test import APIClient
 
 CREATE_USER_URL = reverse("user:create")
 TOKEN_URL = reverse("user:token")
+ME_URL = reverse("user:me")
 
 
 def create_user(**params):
@@ -105,3 +106,49 @@ class PublicUsersApiTest(TestCase):
         res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
         self.assertNotIn("token", res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """test auth is required for users"""
+
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test api endpoints that require auth"""
+
+    def setUp(self):
+        self.user = create_user(
+            email="test@email.com",
+            password="password",
+            name="name"
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test successful retrieval of self profile"""
+
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_user_not_allowed(self):
+        """test cannot update user profile"""
+
+        res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_success(self):
+        """test cannot update user profile"""
+        payload = {"name": "sean", "password": "newpassword"}
+
+        res = self.client.patch(ME_URL, payload)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
