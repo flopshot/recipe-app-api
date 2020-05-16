9578a21f141bea8cd82645315d4d8ca63529568f
seannajera <n112358@gmail.com>
2020-05-16 15:12:12 -0400

    create BaseRecipeAttributeViewSet for DRY

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,34 +6,33 @@ from core.models import Tag, Ingredient
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    """Manages tags in database"""
+class BaseRecipeAttributeViewSet(viewsets.GenericViewSet,
+                                 mixins.ListModelMixin,
+                                 mixins.CreateModelMixin):
+    """Base ViewSet for recipe attributes in database"""
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """return objects for the authenticated user only"""
 
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
+    def perform_create(self, serializer):
+        """create a new object"""
+        serializer.save(user=self.request.user)
 
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+
+class TagViewSet(BaseRecipeAttributeViewSet):
+    """Manages tags in database"""
+
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class IngredientViewSet(BaseRecipeAttributeViewSet):
     """Manages ingredients in the database"""
 
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
-
-    def get_queryset(self):
-        """defines query options for Ingredients endpoint to auth'd user"""
-        return self.queryset.filter(user=self.request.user).order_by("-name")
-
-    def perform_create(self, serializer):
-        """create a new ingredient"""
-        serializer.save(user=self.request.user)
